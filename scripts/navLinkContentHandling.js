96911241301a6fb62a60cc77ea50a19915abcc08
sidvip <sidvip@users.noreply.github.com>
2019-07-15 01:40:23 +0530

    some project changes

--- scripts/navLinkContentHandling.js
+++ scripts/navLinkContentHandling.js
@@ -18,9 +18,9 @@ function addVideoRobotGrasping() {
             <td class="table-heading"><span id="contentHeading"> Kinect View </span></td>
         </tr>
         <tr>
-            <td class="video-support-pane"><iframe class="video-pane" src="https://www.youtube.com/embed/fLyEEp4vjBo?autoplay=0"
+            <td class="video-support-pane"><iframe class="video-pane" src="https://www.youtube.com/embed/fLyEEp4vjBo?autoplay=1"
             frameborder="0" allowfullscreen></iframe></td>
-            <td class="video-support-pane"><iframe class="algo-pane" src="https://www.youtube.com/embed/Wv0Vq-z6vt0?autoplay=0"
+            <td class="video-support-pane"><iframe class="algo-pane" src="https://www.youtube.com/embed/Wv0Vq-z6vt0?autoplay=1"
             frameborder="0" allowfullscreen></iframe></td>
         </tr>
         <tr>
@@ -63,10 +63,108 @@ function addVideoRobotGrasping() {
 }
 
 function addProjectVideo() {
-    document.getElementsByClassName('middle-pane')[0].innerHTML = `<div class="inside-middle-pane" style="text-align:center;"></div>`;
+    document.getElementsByClassName('middle-pane')[0].innerHTML = `<div class="inside-middle-pane" style="text-align:center;position:initial;"></div>`;
     document.getElementsByClassName('inside-middle-pane')[0].innerHTML = `
-    &nbsp;&nbsp;&nbsp;&nbsp;
-        <span id="contentHeading"> Will be posted soon .............. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span>
+    <span id="contentHeading" style="padding-top:100px;"> Manipulator Configuration Space </span>
+    <span id="contentInsideHeading">
+        It was my first animated project in MATLAB. In this we are drawing <a href="https://www.cs.cmu.edu/~motionplanning/lecture/Chap3-Config-Space_howie.pdf"
+         style="text-decoration: none;"> Configuration Space</a> of two link manipulator which is placed among the obstacles. The manipulator has
+         2 degree of freedom that is why the configuration space has 2 dimensional plot. In configuration space, the same color is denoting the 
+         collision of robot with that color obstacle. Rest of the color is simply showing the non-collision zone of robot.
+    </span>
+    <table class="video-table">
+    <tr>
+    <td class="table-heading"><span id="contentHeading"> Manipulator Workspace </span></td>
+    <td class="table-heading"><span id="contentHeading"> Configuration Space </span></td>
+    </tr>
+    <tr>
+    <td class="video-support-pane"><iframe class="video-pane" src="https://www.youtube.com/embed/wIY0vmXf3BE?autoplay=1"
+    frameborder="0" allowfullscreen></iframe></td>
+    <td class="video-support-pane"><iframe class="algo-pane" src="https://www.youtube.com/embed/8lvCUK3cBWA?autoplay=1"
+    frameborder="0" allowfullscreen></iframe></td>
+    </tr>
+    </table>
+
+    <span id="contentHeading" style="padding-top:100px;"> Rapidly-exploring random tree </span>
+    <span id="contentInsideHeading">
+        Here, we have used <a href="http://msl.cs.illinois.edu/~lavalle/papers/Lav98c.pdf"
+        style="text-decoration: none;"> RRT </a>  to plan the path of mobile robot in different workspaces.  Every algorithm is completed in different number 
+        of iterations. We have used only one point tree generation for planning the path for faster planning two point tree generation can be
+        used. 
+    </span>
+    <table class="video-table">
+    <tr style="text-align:center;">
+    <td class="video-support-pane"><iframe class="video-pane" src="https://www.youtube.com/embed/5gV-P4nt048?autoplay=0"
+    frameborder="0" allowfullscreen></iframe> <span class="general-bold">Single Point Tree Generation
+    <br>Number of Iterations : 1000</span></td>
+    <td class="video-support-pane"><iframe class="algo-pane" src="https://www.youtube.com/embed/uUIyWGl_d7w?autoplay=0"
+    frameborder="0" allowfullscreen></iframe> <span class="general-bold">Two Point Tree Generation
+    <br>Number of Iterations : 200</span></td>
+    </tr>
+    <tr style="text-align:center;">
+    <td class="video-support-pane"><iframe class="video-pane" src="https://www.youtube.com/embed/7favRKNxcsk?autoplay=0"
+    frameborder="0" allowfullscreen></iframe> <span class="general-bold">Single Point Tree Generation
+    <br>Number of Iterations : 2500</span></td>
+    <td class="video-support-pane"><iframe class="algo-pane" src="https://www.youtube.com/embed/3zIF-PMZPKc?autoplay=0"
+    frameborder="0" allowfullscreen></iframe> <span class="general-bold">Single Point Tree Generation
+    <br>Number of Iterations : 1200</span></td>
+    </tr>
+    <tr style="text-align:center;">
+    <td class="video-support-pane"><iframe class="video-pane" src="https://www.youtube.com/embed/FuEZ1Uxf97Q?autoplay=0"
+    frameborder="0" allowfullscreen></iframe> <span class="general-bold">Single Point Tree Generation
+    <br>Number of Iterations : 4825</span></td>
+    <td class="video-support-pane"> <span class="general-bold">NoI depends on the random 
+    points sampling positions so, we get different values at almost every run.</span></td>
+    </tr>
+    </table>
+
+    <span id="contentHeading" style="padding-top:100px;"> Artificial Potential Field Path Planning</span>
+    <span id="contentInsideHeading">
+        Here, we have used <a href="https://www.cs.mcgill.ca/~hsafad/robotics/"
+        style="text-decoration: none;"> Potential Field  Algorithm</a>  to plan the path of mobile robot in different workspaces.  In this
+        algorithm, target and robot has force of attraction while the robot and obstacle has force of repulsion. If these two forces come
+        in line then the problem of local minima occurs.</span>
+    <table class="video-table">
+    <tr>
+    <td class="table-heading"><span id="contentHeading"> Manipulator Workspace </span></td>
+    <td class="table-heading"><span id="contentHeading"> Configuration Space </span></td>
+    </tr>
+    <tr>
+    <td class="video-support-pane"><iframe class="video-pane" src="https://www.youtube.com/embed/ZAFhE9vPAZY?autoplay=0"
+    frameborder="0" allowfullscreen></iframe></td>
+    <td class="video-support-pane"><iframe class="algo-pane" src="https://www.youtube.com/embed/B1bwDn2GzIc?autoplay=0"
+    frameborder="0" allowfullscreen></iframe></td>
+    </tr>
+    <tr>
+    <td class="video-support-pane"><iframe class="video-pane" src="https://www.youtube.com/embed/3szX17D8x84?autoplay=0"
+    frameborder="0" allowfullscreen></iframe></td>
+    <td class="video-support-pane"> <span class="general-bold"> Here it is easily visible that both of the forces i.e, attraction
+    and repulsion are inline so robot is stuck at local minima. One of the ways to eliminate it using tangetial force.</span></td>
+    </tr>
+
+    </table>
+
+    <span id="contentHeading" style="padding-top:100px;"> 3 Dimensional Path Planning</span>
+    <span id="contentInsideHeading">
+        Here, we have used <a href="https://www.cs.mcgill.ca/~hsafad/robotics/"
+        style="text-decoration: none;"> Potential Field  Algorithm</a>  to plan the path of aerial robot in 3 dimensional space.  We have shown
+        the path planning for two different initial points to target point.  Potential field algorithms used the heuristics to deicide the 
+        factors used in their potential functions, if not chosen correctly, chances of high repulsion or weird behaviour is found in generated path.
+        Due to poor selection a high amount of jerks are encountered by robot in workspace.
+        </span>
+    <table class="video-table">
+    <tr>
+    <td class="table-heading"><span id="contentHeading"> Manipulator Workspace </span></td>
+    <td class="table-heading"><span id="contentHeading"> Configuration Space </span></td>
+    </tr>
+    <tr>
+    <td class="video-support-pane"><iframe class="video-pane" src="https://www.youtube.com/embed/6AuAL4lhkGk?autoplay=0"
+    frameborder="0" allowfullscreen></iframe></td>
+    <td class="video-support-pane"><iframe class="algo-pane" src="https://www.youtube.com/embed/Af1Ecq0HD9Y?autoplay=0"
+    frameborder="0" allowfullscreen></iframe></td>
+    </tr>
+    </table>
+
     `;
 }
 
